ecd34003839b52ec6785c4cf3e822562b639d49d
YoshiCrystal9 <89108143+YoshiCrystal9@users.noreply.github.com>
2023-04-17 09:46:45 +0200

    Update data.js

--- data.js
+++ data.js
@@ -40,6 +40,79 @@ const splat2Maps = [
 ];
 
 const splat3Maps = [
-    "None lol, select Splatoon 2 in Preferences to get Gamblitz content",
-    "go to the original page to get the splat 3 maps"
+    "The Reef",
+
+    "Musselforge Fitness",
+
+    "Starfish Mainstage",
+
+    "Humpback Pump Track",
+
+    "Inkblot Art Academy",
+
+    "Sturgeon Shipyard",
+
+    "Moray Towers (Splatoon 2)",
+
+    "Port Mackerel",
+
+    "Manta Maria",
+
+    "Kelp Dome (Splatoon 2)",
+
+    "Snapper Canal",
+
+    "Blackbelly Skatepark (Splatoon 2)",
+
+    "MakoMart",
+
+    "Walleye Warehouse",
+
+    "Shellendorf Institute",
+
+    "Arowana Mall (Splatoon 2)",
+
+    "Goby Arena",
+
+    "Piranha Pit (Splatoon 2)",
+
+    "Camp Triggerfish (Splatoon 2)",
+
+    "Wahoo World",
+
+    "New Albacore Hotel",
+
+    "Ancho-V Games (Splatoon 2)",
+
+    "Skipper Pavilion",
+
+    "Old Urchin Underpass",
+
+    "New Urchin Underpass",
+
+    "Saltspray Rig",
+
+    "Arowana Mall (Splatoon 1)",
+
+    "Blackbelly Skatepark (Splatoon 1)",
+
+    "Port Mackerel (Splatoon 1)",
+
+    "Bluefin Depot",
+
+    "Moray Towers (Splatoon 1)",
+
+    "Camp Triggerfish (Splatoon 1)",
+
+    "Flounder Heights",
+
+    "Hammerhead Bridge",
+
+    "Museum d'Alfonsino",
+
+    "Mahi-Mahi Resort",
+
+    "Piranha Pit (Splatoon 1)",
+
+    "Ancho-V Games (Splatoon 1)"
 ];
